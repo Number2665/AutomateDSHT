62a66d56b018fc1b7a88bf032e7408f3460e27d4
Number2665 <69061554+Number2665@users.noreply.github.com>
2023-08-16 18:01:22 -0600

    Create makeChallonge.py

---
+++ makeChallonge.py
@@ -0,0 +1,14 @@
+# Well, we learn to do by doing
+# This is (hopefully?) the python script I want that automates my weekly
+# Maybe it ends up being in javascript later
+
+# Goals for this:
+# This should take information from a text file that contains the following: 
+# The current D-SHT #, the default schedule information, and the JSON template
+# It shows these in the standard announcement form, and requests any adjustment
+# Then replace any JSON placeholders for each game
+#   (API KEY, D-SHT #, The date + start time, and the game played)
+# Finally call the challonge API to create the brackets
+
+
+# STEP ONE: How do I write something in python...
