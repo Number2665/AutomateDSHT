122f0115957bbd1840ef15f60f97f5b32a370044
Number2665 <69061554+Number2665@users.noreply.github.com>
2023-08-29 18:05:30 -0600

    Update makeChallonge.py

--- Brackets/makeChallonge.py
+++ Brackets/makeChallonge.py
@@ -1,7 +1,3 @@
-# Well, we learn to do by doing
-# This is (hopefully?) the python script I want that automates my weekly
-# Maybe it ends up being in javascript later
-
 # Goals for this:
 # This should take information from a text file that contains the following: 
 # The current D-SHT #, the default schedule information, and the JSON template
@@ -11,4 +7,36 @@
 # Finally call the challonge API to create the brackets
 
 
-# STEP ONE: How do I write something in python...
+#! Python 3
+# makeChallonge.py is to make challonge brackets for the weekly events
+
+import requests, json
+
+# The current D-SHT number, load in somehow----------
+dshtNumber = # Load this in from the hidden data json perhaps?
+# The data getting sent with the post request, I think it's the API key?----------
+data = # load the API key here
+# The URL for the Challonge API, maybe worth also loading in
+apiURL = 'https://api.challonge.com/v1/tournaments.json'
+
+# The template json to start with, load in api_key somehow from somewhere private----------
+challongeTemplate = {
+    "tournament":{
+        "name": "D-SHT (NUMBER) (EVENT TYPE)",
+        "tournament_type": "double elimination", 
+        "url": "DSHT(NUMBER)(EVENT TYPE)",
+        "description": "Maybe I use this??",
+        "start_at": "(EVENT TYPE START TIME)"    
+    }
+}
+
+# Make something to create the name, url, and start time----------
+
+# Function to make an individual challonge bracket given the event type and start time----------
+makeBracket(eventType, startTime):
+    challongeTemplate[name] = "D-SHT " + str(dshtNumber) + " " + eventType
+    challongeTemplate[url] = "DSHT"+str(dshtNumber)+eventType
+    challongeTemplate[start_at] = 
+    challongeJson = json.dumps(challongeTemplate)
+    # requests.post(apiURL, data, challongeJson)
+
